817dcd8ebf91ad3cbbc01bbe1b81452eff8c4e7a
guodongqi <guodongqi2@huawei.com>
2022-05-11 10:54:34 +0800

    test: add framework fuzztest
    Signed-off-by: guodongqi <guodongqi2@huawei.com>

--- test/fuzztest/hdimanager/unloaddevice_fuzzer/unloaddevice_fuzzer.cpp
+++ test/fuzztest/hdimanager/unloaddevice_fuzzer/unloaddevice_fuzzer.cpp
@@ -6,20 +6,29 @@
  * See the LICENSE file in the root of this repository for complete details.
  */
 
-#include "loaddevice_fuzzer.h"
+#include "unloaddevice_fuzzer.h"
 #include <cstddef>
 #include <cstdint>
+#include <hdf_base.h>
+#include <hdi_base.h>
 #include "idevmgr_hdi.h"
 
 using namespace OHOS::HDI::DeviceManager::V1_0;
 
+static constexpr const char *TEST_SERVICE_NAME = "sample_driver_service";
+
 namespace OHOS {
-bool LoadDeviceFuzzTest(const uint8_t *data, size_t size)
+sptr<IDeviceManager> g_devicemanager = IDeviceManager::Get();
+
+bool UnloadDeviceFuzzTest(const uint8_t *data, size_t size)
 {
     bool result = false;
     std::string serviceName((const char *)data);
-    sptr<IDeviceManager> devicemanager = IDeviceManager::Get();
-    if (!devicemanager->LoadDevice(serviceName)) {
+    if (g_devicemanager != nullptr && g_devicemanager->LoadDevice(TEST_SERVICE_NAME) == HDF_SUCCESS) {
+        result = true;
+    }
+
+    if (g_devicemanager != nullptr && g_devicemanager->UnloadDevice(serviceName) == HDF_SUCCESS) {
         result = true;
     }
     return result;
@@ -28,6 +37,6 @@ bool LoadDeviceFuzzTest(const uint8_t *data, size_t size)
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
-    OHOS::LoadDeviceFuzzTest(data, size);
-    return 0;
+    OHOS::UnloadDeviceFuzzTest(data, size);
+    return HDF_SUCCESS;
 }
